ed68399fe672052c4e28e76cd981a2a4c110b095
HouCemELLOUZE <167681850+HouCemELLOUZE@users.noreply.github.com>
2024-04-21 14:29:22 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var typedMultiplePh = new Typed(".multiple-ph", {
+      strings: [" Developpeur Front-End " , "  Developpeur Back-End" , "  Developpeur Full Stack"],
+      typeSpeed: 50,
+      backSpeed: 0,
+      backDelay: 1000,
+      loop: true
+    })
+  }
+);
